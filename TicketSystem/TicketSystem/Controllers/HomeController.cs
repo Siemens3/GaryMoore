419fe65cc0d732634f6cf2e9a235410f08e7d9d9
Stephen kenny <stephenkenny@outlook.ie>
2015-11-08 23:24:07 +0000

    main file

---
+++ TicketSystem/TicketSystem/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicketSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Merchandise()
+        {
+            return View();
+        }
+
+        public ActionResult WhyBrighton()
+        {
+            return View();
+        }
+        public ActionResult GaryMoore()
+        {
+            return View();
+        }
+        public ActionResult PlacesOfInterest()
+        {
+            return View();
+        }
+        public ActionResult PastGatherings()
+        {
+            return View();
+        }
+        public ActionResult UsefulLinks()
+        {
+            return View();
+        }
+    }
+}
